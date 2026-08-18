bf4e95058c78db894a58b1d326b4ce096bfca2e5
Alan Lyu <alanlyu4work@outlook.com>
2026-08-18 16:31:49 +0800

    [Docker] Update Docker Security Settings

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -2,25 +2,32 @@
 set -eu
 
 DSH_DATA_DIR=/data/dsh
+PNPM_HOME="${DSH_DATA_DIR}/pnpm"
+
+umask 077
 
 install -d -m 0755 /run/sshd
-install -d -o developer -g developer -m 0755 "${DSH_DATA_DIR}"
+install -d -o developer -g developer -m 0700 "${DSH_DATA_DIR}"
 chown -R developer:developer "${DSH_DATA_DIR}"
+chmod 0700 "${DSH_DATA_DIR}"
+install -d -o developer -g developer -m 0700 "${PNPM_HOME}"
 install -d -o developer -g developer -m 0755 /home/developer/workspace
 
-/usr/sbin/sshd
+/usr/sbin/sshd -e
 
-# dsh 固定监听容器回环地址；由 socat 暴露一个仅供 Docker 端口映射的桥接端口。
+# DSH listens on the container loopback address; socat exposes a bridge port for Docker mapping.
 socat \
     TCP-LISTEN:3081,bind=0.0.0.0,reuseaddr,fork \
     "TCP:127.0.0.1:${DSH_PORT:-3080}" &
 
 cd /home/developer/.dsh-launcher
 
-exec sudo -u developer -H env \
+exec /usr/sbin/runuser -u developer -- env \
+    HOME=/home/developer \
     CARGO_HOME=/home/developer/.cargo \
     RUSTUP_HOME=/usr/local/rustup \
-    PATH="/home/developer/.cargo/bin:/usr/local/cargo/bin:${PATH}" \
+    PNPM_HOME="${PNPM_HOME}" \
+    PATH="/home/developer/.cargo/bin:/usr/local/cargo/bin:${PATH}:${PNPM_HOME}" \
     DSH_HOME="${DSH_DATA_DIR}" \
     DSH_PORT="${DSH_PORT:-3080}" \
     "$@"
